b4ea0b132bf4770f894fad33bad88a3240419a51
Per Nordlöw <per.nordlow@gmail.com>
2014-03-05 14:45:11 +0100

    Add velocity

--- units_SI.hpp
+++ units_SI.hpp
@@ -21,8 +21,9 @@ UNITS_MAKE_BASE_DIMENSION(_luminous_intensity,_cd,7);
 
 UNITS_MAKE_SCALAR_DIMENSION(_scalar);
 
-UNITS_MAKE_DERIVED_DIMENSION(_acceleration,_ms2,_m/(_s*_s));
-UNITS_MAKE_DERIVED_DIMENSION(_force,_N,_kg*_ms2);
+UNITS_MAKE_DERIVED_DIMENSION(_velocity,_mps,_m/_s);
+UNITS_MAKE_DERIVED_DIMENSION(_acceleration,_mps2,_m/(_s*_s));
+UNITS_MAKE_DERIVED_DIMENSION(_force,_N,_kg*_mps2);
 UNITS_MAKE_DERIVED_DIMENSION(_energy,_J,_N*_m);
 UNITS_MAKE_DERIVED_DIMENSION(_electric_potential,_V,_kg*(_m*_m)/(_s*_s*_s*_A));
 UNITS_MAKE_DERIVED_DIMENSION(_resistance,_ohms,_V/_A);

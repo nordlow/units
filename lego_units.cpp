3b1ebbb5b4c965d4391b7fbb611e7b3d11378584
Per Nordlöw <per.nordlow@gmail.com>
2014-03-05 11:31:15 +0100

    C instead of complex_type

--- lego_units.cpp
+++ lego_units.cpp
@@ -14,9 +14,9 @@ work(const quantity<_force>& F, const quantity<_length>& dx)
 int main()
 {
     /// Test calculation of work.
-    quantity<_force>     F = 2.0_N; // Define a quantity of force.
-    quantity<_length>    dx = 2.0_m;       // and a distance,
-    quantity<_energy>    E = work(F,dx);    // and calculate the work done.
+    quantity<_force>  F = 2.0_N; // Define a quantity of force.
+    quantity<_length> dx = 2.0_m;       // and a distance,
+    quantity<_energy> E = work(F,dx);    // and calculate the work done.
 
     std::cout << "F  = " << F << std::endl
               << "dx = " << dx << std::endl
@@ -24,12 +24,12 @@ int main()
               << std::endl;
 
     /// Test and check complex quantities.
-    typedef std::complex<double> complex_type; // double real and imaginary parts.
+    typedef std::complex<double> C; // double real and imaginary parts.
 
     // Define some complex electrical quantities.
-    quantity<_electric_potential, complex_type> v = complex_type(12.5, 0.0) * _V;
-    quantity<_current, complex_type>            i = complex_type(3.0, 4.0) * _A;
-    quantity<_resistance, complex_type>         z = complex_type(1.5, -2.0) * _ohms;
+    quantity<_electric_potential, C> v = C(12.5, 0.0) * _V;
+    quantity<_current, C>            i = C(3.0, 4.0) * _A;
+    quantity<_resistance, C>         z = C(1.5, -2.0) * _ohms;
 
     std::cout << "V   = " << v << std::endl
               << "I   = " << i << std::endl
